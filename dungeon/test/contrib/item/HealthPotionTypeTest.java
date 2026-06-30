fc3a45203a807c5c2d65e87eb4358f6a9b3cd5b6
mbeu98 <mikabeu@yahoo.de>
2026-06-30 18:09:42 +0200

    again spotless corrections

--- dungeon/test/contrib/item/HealthPotionTypeTest.java
+++ dungeon/test/contrib/item/HealthPotionTypeTest.java
@@ -1,18 +1,13 @@
 package contrib.item;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import org.junit.jupiter.api.Test;
 
 import java.util.Optional;
+import org.junit.jupiter.api.Test;
 
-
-
-/**
- * Test class
- */
-
+/** Test class */
 public class HealthPotionTypeTest {
-
 
   @Test
   void getHealAmount_shouldReturnCorrectValues() {
@@ -25,37 +20,29 @@ public class HealthPotionTypeTest {
   void fromHealAmount_withInput7_shouldReturnCorrectType() {
     assertEquals(Optional.of(HealthPotionType.WEAK), HealthPotionType.fromHealAmount(7));
   }
+
   @Test
   void fromHealAmount_withInput15_shouldReturnCorrectType() {
     assertEquals(Optional.of(HealthPotionType.NORMAL), HealthPotionType.fromHealAmount(15));
   }
+
   @Test
   void fromHealAmount_withInput30_shouldReturnCorrectType() {
     assertEquals(Optional.of(HealthPotionType.GREATER), HealthPotionType.fromHealAmount(30));
   }
 
-
   @Test
   void fromHealAmount_withNegativeInputValue_shouldReturnEmpty() {
     assertTrue(HealthPotionType.fromHealAmount(-1).isEmpty());
   }
+
   @Test
   void fromHealAmount_with0InputValue_shouldReturnEmpty() {
     assertTrue(HealthPotionType.fromHealAmount(0).isEmpty());
   }
+
   @Test
   void fromHealAmount_withHighInputValue_shouldReturnEmpty() {
     assertTrue(HealthPotionType.fromHealAmount(999).isEmpty());
   }
-
-
-
-
-
-
-
-
-
-
-
 }
