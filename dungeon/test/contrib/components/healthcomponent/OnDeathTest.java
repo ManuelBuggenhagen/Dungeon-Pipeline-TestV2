2e58d9b9b708758a4f2fbf8e99ab6a022993bcec
JCodevanDamme <jcodevandamme@gmail.com>
2026-07-01 11:15:41 +0200

    #108 HealthComponent - onDeath; Added Tests.

---
+++ dungeon/test/contrib/components/healthcomponent/OnDeathTest.java
@@ -0,0 +1,56 @@
+package contrib.components.healthcomponent;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+import contrib.components.HealthComponent;
+import core.Entity;
+import java.util.function.Consumer;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+/** Tests for {@link HealthComponent#onDeath(Consumer)}. */
+@SuppressWarnings("unchecked")
+public class OnDeathTest {
+
+  HealthComponent healthComponent;
+
+  Entity entity;
+
+  Consumer<Entity> callback;
+
+  @BeforeEach
+  void setup() {
+    healthComponent = new HealthComponent();
+    entity = Mockito.mock(Entity.class);
+    callback = Mockito.mock(Consumer.class);
+  }
+
+  @Test
+  void onDeath_passingValidCallbackAndNoCallbackSet_callbackSuccessfullySet() {
+    healthComponent.onDeath(callback);
+    healthComponent.triggerOnDeath(entity);
+
+    verify(callback, times(1));
+  }
+
+  @Test
+  void onDeath_passingValidCallbackAndCallbackSet_callbackSuccessfullyOverwritten() {
+    Consumer<Entity> overwriteCallback = Mockito.mock(Consumer.class);
+    healthComponent.onDeath(callback);
+    healthComponent.onDeath(overwriteCallback);
+
+    healthComponent.triggerOnDeath(entity);
+
+    verify(callback, times(0));
+  }
+
+  @Test
+  void onDeath_passingNull_callbackSetToNull() {
+    healthComponent.onDeath(null);
+
+    assertThrows(NullPointerException.class, () -> healthComponent.triggerOnDeath(entity));
+  }
+}
