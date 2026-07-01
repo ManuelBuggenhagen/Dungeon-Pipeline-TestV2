9111b11184a3c693b4a085d70fd03f57c9fc5c19
JCodevanDamme <jcodevandamme@gmail.com>
2026-07-01 10:02:20 +0200

    #107 HealthComponent - triggerOnDeath; Added Tests.

---
+++ dungeon/test/contrib/components/healthcomponent/TriggerOnDeathTest.java
@@ -0,0 +1,58 @@
+package contrib.components.healthcomponent;
+
+import contrib.components.HealthComponent;
+import core.Entity;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.function.Consumer;
+
+import static org.junit.jupiter.api.Assertions.assertSame;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.mock;
+
+/**
+ * Tests for {@link HealthComponent#triggerOnDeath}.
+ */
+public class TriggerOnDeathTest {
+
+  private Entity entity;
+
+  private HealthComponent healthComponent;
+
+  @BeforeEach
+  void setup() {
+    entity = mock(Entity.class);
+  }
+
+  @Test
+  void triggerOnDeath_entitySetAndOnDeathCallbackSet_callbackExecutedOnEntity() {
+    AtomicReference<Entity> received = new AtomicReference<>();
+    Consumer<Entity> callback = received::set;
+    healthComponent = new HealthComponent(1, callback);
+
+    healthComponent.triggerOnDeath(entity);
+
+    assertSame(entity, received.get());
+  }
+
+  @Test
+  void triggerOnDeath_entityIsNullAndOnDeathCallbackSet_callbackExecutedOnNull() {
+    AtomicReference<Entity> received = new AtomicReference<>();
+    Consumer<Entity> callback = received::set;
+    healthComponent = new HealthComponent(1, callback);
+
+    healthComponent.triggerOnDeath(null);
+
+    assertNull(received.get());
+  }
+
+  @Test
+  void triggerOnDeath_onDeathCallbackIsNull_throwsNullPointerException() {
+    healthComponent = new HealthComponent(1, null);
+
+    assertThrows(NullPointerException.class, ()-> healthComponent.triggerOnDeath(entity));
+  }
+}
