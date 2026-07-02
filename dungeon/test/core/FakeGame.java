0f623d8a2e264f3225dbb057b7e25e73bd3312bc
mbeu98 <mikabeu@yahoo.de>
2026-07-02 15:49:25 +0200

    Implemented a Fake Game class

---
+++ dungeon/test/core/FakeGame.java
@@ -0,0 +1,47 @@
+package core;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Stream;
+
+/** for test purposes to be independent of the whole framework */
+public class FakeGame {
+
+  private final Set<Entity> entities = new HashSet<>();
+
+
+
+  public Entity add(Entity entity) {
+    entities.add(entity);
+    return entity;
+  }
+
+
+  public Entity remove(Entity entity) {
+    entities.remove(entity);
+    return entity;
+  }
+
+
+
+  public Stream<Entity> allEntities() {
+    return entities.stream();
+  }
+
+
+  public final Stream<Entity> filteredEntityStream(
+    Class<? extends Component>... components) {
+
+    return entities.stream()
+      .filter(entity ->
+        java.util.Arrays.stream(components)
+          .allMatch(entity::isPresent)
+      );
+  }
+
+
+  public void clear() {
+    entities.clear();
+  }
+}
+
